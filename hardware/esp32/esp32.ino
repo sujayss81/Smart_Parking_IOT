d6d31db16eba87d5f8cf42af6fdd03982e432e54
Sujay Shenoy <sujay.ss81@gmail.com>
2021-04-05 17:46:40 +0530

    Wifi and connection to server code

--- hardware/esp32/esp32.ino
+++ hardware/esp32/esp32.ino
@@ -1,3 +1,7 @@
+#include<WiFi.h>
+#include<WiFiClient.h>
+#include<String.h>
+
 const int trigPin4 = 4;
 const int echoPin34 = 34;
 
@@ -16,10 +20,111 @@ const int echoPin18 = 18;
 const int trigPin19 = 19;
 const int echoPin21 = 21;
 
+const char* ssid = "TP-Link_2.4";
+const char* pass = "tplink@2.4hz";
+const char* host = "192.168.1.47";
+
 int calculateDistance(int trig,int echo);
+void initPins();
+String readUltrasonic();
+void connectWifi();
+void checkWifi();
+void connectToServer();
+void checkServer();
+void mainProcess(WiFiClient client);
+String getCommand(String s);
+String getOption(String s);
+void handleRequest(WiFiClient client,char com,char opt);
 
 void setup() {
   Serial.begin(9600);
+  delay(3000);
+  initPins();
+  connectWifi();
+  connectToServer();
+}
+
+void loop() {
+  
+ 
+}
+
+void connectToServer()
+{
+  Serial.println("Connecting to Server");
+  WiFiClient client;
+  bool c = client.connect(host,8000);
+  while(!c){
+    Serial.print("*");
+    c = client.connect(host,8000);
+    delay(1000);
+  }
+  Serial.println();
+  Serial.println("Server Connected");
+  digitalWrite(2,HIGH);
+  mainProcess(client);
+  
+}
+
+void mainProcess(WiFiClient client){
+  while(1)
+  {
+    String dat = "";
+    while(client.available())
+    {
+      dat += client.readString();
+    }
+    if(dat.length() > 0 )
+    {
+      Serial.println("Server Requested");
+      char str[1][50];
+      dat.toLowerCase();
+      dat.toCharArray(str[0],50);
+      Serial.println(dat);
+      handleRequest(client,str[0][0],str[0][1]);
+    }
+  }
+}
+
+void handleRequest(WiFiClient client,char com,char opt){
+  switch(com)
+  {
+    case '1' : client.print(readUltrasonic());
+                    break;
+    default: Serial.println("Invalid Request");
+             client.println("Invalid Request"); 
+             break;
+  }
+}
+
+
+
+String readUltrasonic(){
+  Serial.println("Reading Sensors");
+  int d[6];
+  d[0] = calculateDistance(trigPin4,echoPin34); 
+  d[1] = calculateDistance(trigPin5,echoPin35); 
+  d[2] = calculateDistance(trigPin13,echoPin36); 
+  d[3] = calculateDistance(trigPin14,echoPin39);
+  d[4] = calculateDistance(trigPin15,echoPin18); 
+  d[5] = calculateDistance(trigPin19,echoPin21);
+
+  String r="";
+  r+=d[0];
+  for(int i=1;i<6;i++)
+  {
+    r+=",";
+    r+=d[i];
+  }
+  Serial.println(r);
+
+  return r;
+}
+
+void initPins(){
+  Serial.println("Initializing Pins");
+
+  pinMode(2,OUTPUT);
   
   pinMode(trigPin4, OUTPUT);
   pinMode(echoPin34, INPUT);
@@ -40,23 +145,6 @@ void setup() {
   pinMode(echoPin21, INPUT);
 }
 
-void loop() {
-  int a[6] = {0,0,0,0,0,0};
-  a[0] = calculateDistance(trigPin4,echoPin34);
-  a[1] = calculateDistance(trigPin5,echoPin35);
-  a[2] = calculateDistance(trigPin13,echoPin36);
-  a[3] = calculateDistance(trigPin14,echoPin39);
-  a[4] = calculateDistance(trigPin15,echoPin18);
-  a[5] = calculateDistance(trigPin19,echoPin21);
-  
-  for(int i=0;i<6;i++){
-    Serial.print(a[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
-  delay(1000);
-}
-
 int calculateDistance(int trig,int echo){
   
   digitalWrite(trig,LOW);
@@ -70,3 +158,28 @@ int calculateDistance(int trig,int echo){
   return (int)(duration*0.034/2);
   
 }
+
+void connectWifi(){
+  digitalWrite(2,HIGH);
+  WiFi.mode(WIFI_STA);
+  Serial.println("Connecting to Wifi");
+  Serial.print("SSID = ");
+  Serial.println(ssid);
+  WiFi.begin(ssid,pass);
+  Serial.println();
+  while( WiFi.status() != WL_CONNECTED )
+  {
+    Serial.print("*");
+    delay(100);
+  }
+  Serial.println();
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("Wifi Connected");
+    Serial.print("IP = ");
+    Serial.println(WiFi.localIP());
+    digitalWrite(2,LOW);
+  }
+}
+
+void checkWifi(){}
+void checkServer(){}
