43e686f9d1320866231a1443a278b70f96e2c376
Sujay Shenoy <sujay.ss81@gmail.com>
2021-05-20 21:55:44 +0530

    deleted android_app directory

--- hardware/esp32/esp32.ino
+++ hardware/esp32/esp32.ino
@@ -3,13 +3,17 @@
 #include <ESP32Servo.h>
 #include<String.h>
 WiFiClient client;
+Servo s1,s2,s3,s4,s5;
 
 class Slot{ 
   public:
-  int trig,echo,dist;
-  Slot(int trigPin,int echoPin){
+  int trig,echo,dist,group;
+  Servo servo;
+  Slot(int trigPin,int echoPin,int servoPin,int grp){
     trig = trigPin;
     echo = echoPin;
+    group = grp;
+    servo.attach(servoPin);
   }
 };
 
@@ -17,31 +21,40 @@ class Slot{
 
 const int trigPin4 = 4;
 const int echoPin34 = 34;
+const int servoPin16 = 16;
 
 const int trigPin5 = 5;
 const int echoPin35 = 35;
+const int servoPin17 = 17;
 
 const int trigPin13 = 13;
 const int echoPin36 = 36;
+const int servoPin18 = 18;
 
 const int trigPin14 = 14;
 const int echoPin39 = 39;
+const int servoPin19 = 19;
 
 const int trigPin15 = 15;
 const int echoPin18 = 18;
+const int servoPin21 = 21;
 
-Slot s[] = {Slot(trigPin4,echoPin34),Slot(trigPin5,echoPin35),Slot(trigPin13,echoPin36),Slot(trigPin14,echoPin39),Slot(trigPin15,echoPin18)};
-
-//const int trigPin19 = 19;
-//const int echoPin21 = 21;
+Slot s[] = {
+  Slot(trigPin4,echoPin34,servoPin16,1),
+  Slot(trigPin5,echoPin35,servoPin17,1),
+  Slot(trigPin13,echoPin36,servoPin18,2),
+  Slot(trigPin14,echoPin39,servoPin19,2),
+  Slot(trigPin15,echoPin18,servoPin21,2)
+  };
 
 const char* ssid = "TP-Link_2.4";
 const char* pass = "tplink@2.4hz";
-const char* host = "192.168.0.105";
+const char* host = "192.168.0.106";
 
 int calculateDistance(int trig,int echo);
 void initPins();
 String readUltrasonic();
+String reserveSpot(int spotNumber);
 void connectWifi();
 void checkWifi();
 void connectToServer();
@@ -51,12 +64,14 @@ String getCommand(String s);
 String getOption(String s);
 void handleRequest(WiFiClient client,char com,char opt);
 
+
 void setup() {
   Serial.begin(9600);
   delay(3000);
   initPins();
   connectWifi();
   connectToServer();
+ 
 }
 
 void loop() {
@@ -104,12 +119,33 @@ void mainProcess(WiFiClient client){
   
 }
 
+String reserveSpot(int spotNumber){
+  spotNumber = spotNumber -48; //ascii conversion
+  Serial.print("Reserving ");
+  Serial.println(spotNumber-1);
+  int actualSpot = spotNumber-1;
+  int grp = s[actualSpot].group;
+  if(grp == 1){
+    s[actualSpot].servo.write(180);
+  }
+  else{
+    s[actualSpot].servo.write(90);
+  }
+    return "ok";
+}
+
 void handleRequest(WiFiClient client,char com,char opt){
+  Serial.print("Command : ");
+  Serial.println(com);
+  Serial.print("Option : ");
+  Serial.println(opt);
   switch(com)
   {
     case '1' : client.print(readUltrasonic());
                     break;
-    default: Serial.println("Invalid Request");
+    case '2' : client.print(reserveSpot(opt));
+                break;
+    default: Serial.print("Invalid Request");
              client.println("Invalid Request"); 
              break;
   }
@@ -137,15 +173,27 @@ String readUltrasonic(){
 }
 
 void initPins(){
-  Serial.println("Initializing Pins");
-
+  Serial.println("Initializing Componenets");
   pinMode(2,OUTPUT);
 
   for(int i=0;i<5;i++){
     pinMode(s[i].trig,OUTPUT);
     pinMode(s[i].echo,INPUT);
+    if(s[i].group == 1){
+      s[i].servo.write(90);
+      delay(1000);
+      s[i].servo.write(180);
+      delay(1000);
+      s[i].servo.write(90);
+    }
+    else{
+      s[i].servo.write(0);
+      delay(1000);
+      s[i].servo.write(90);
+      delay(1000);
+      s[i].servo.write(0);
+    }
   }
-  
 }
 
 int calculateDistance(int trig,int echo){
