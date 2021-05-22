df688b324d3a4c160a435b51969082e659ee6ff7
Saneeth99 <66729256+Saneeth99@users.noreply.github.com>
2021-05-22 17:36:06 +0530

    updated get and post method of sign up screen

--- smart_parking_app/lib/screens/registrationScreen.dart
+++ smart_parking_app/lib/screens/registrationScreen.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'package:smart_parking_app/services/networking.dart';
 
 enum Gender { male, female }
 
@@ -13,7 +14,7 @@ class RegistrationScreen extends StatefulWidget {
   String email;
   String password;
   String dob = '';
-  String gender;
+  String gender = '';
 
   static const id = 'registration_screen';
   @override
@@ -23,35 +24,11 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   String dob = 'Select Date';
   SharedPreferences prefs;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  Networking net = Networking();
 
-  void postRequest({String urlLabel, Map body}) async {
-    String sendingBody = jsonEncode(body);
-    print(sendingBody);
-    String token;
-    http.Response res = await http.post(Uri.parse('$hostUrl/$urlLabel'),
-        body: sendingBody, headers: {'Content-Type': 'application/json'});
-    if (res.statusCode == 200) {
-      token = res.headers['x-auth-token'];
-      print(token);
-      prefs.setString('token', token);
-      http.Response res2 = await http.get(Uri.parse('$hostUrl/me'),
-          headers: {'x-auth-token': token, 'Content-Type': 'application/json'});
-      if (res2.statusCode == 200) {
-        // print(res2);
-        print(res2.body);
-      } else if (res2.statusCode == 400) {
-        print('authentication problem');
-        // print(res.body);
-      } else {
-        print('server error');
-      }
-    } else if (res.statusCode == 400) {
-      print('post request authentication error');
-    } else {
-      print('post request server error');
-      print(res.statusCode);
-    }
-  }
+
+
 
   void initSharedPref() async {
     prefs = await SharedPreferences.getInstance();
@@ -62,7 +39,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     initSharedPref();
   }
 
-  void registerUser() {
+  void registerUser() async{
+    String token='';
+    bool flag = false;
     Map body = {
       'name': widget.name,
       'email': widget.email,
@@ -71,7 +50,30 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       'gender': widget.gender,
     };
     print(body);
-    postRequest(urlLabel: 'signup', body: body);
+    http.Response res = await net.postRequest(urlLabel: 'signup', body: body);
+    if (res.statusCode == 200) {
+      token = res.headers['x-auth-token'];
+      print(token);
+      prefs.setString('token', token);
+      prefs.clear();
+    }else {
+      print('server error');
+      flag = true;
+      print(jsonDecode(res.body));
+    }
+    if(!flag) {
+      http.Response res2 = await net.getRequest(
+          urlLabel: 'me', token: token, params: '');
+      if (res2.statusCode == 200) {
+        // print(res2);
+        print(res2.body);
+        //write navigator push code here
+      } else {
+        print('server error');
+        print(jsonDecode(res2.body));
+      }
+    }
+
   }
 
   @override
@@ -80,159 +82,202 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       appBar: AppBar(
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Container(
-                    child: Image(
-                      image: AssetImage('images/iconCar.png'),
-                      height: 100.0,
-                      // width: 200.0,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Hero(
+                      tag: 'logo',
+                      child: Container(
+                        child: Image(
+                          image: AssetImage('images/iconCar.png'),
+                          height: 100.0,
+                          // width: 200.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                width: 15,
-              ),
+                  SizedBox(
+                    width: 15,
+                  ),
 
-              // Text(
-              //   'Fill the form',
-              //   style: TextStyle(
-              //     fontSize: 40,
-              //   ),
-              // )
-            ],
-          ),
-          Text(
-            'REGISTER',
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: kInputDecoration.copyWith(hintText: 'Name'),
-              keyboardType: TextInputType.name,
-              onChanged: (value) {
-                widget.name = value;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: kInputDecoration.copyWith(hintText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                widget.email = value;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              obscureText: true,
-              decoration: kInputDecoration.copyWith(hintText: 'Password'),
-              onChanged: (value) {
-                widget.password = value;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              children: [
-                Text('Date of Birth:'),
-                SizedBox(
-                  width: 50.0,
+                  // Text(
+                  //   'Fill the form',
+                  //   style: TextStyle(
+                  //     fontSize: 40,
+                  //   ),
+                  // )
+                ],
+              ),
+              Text(
+                'REGISTER',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                Material(
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: kInputDecoration.copyWith(hintText: 'Name'),
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) {
+                    widget.name = value;
+                  },
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Enter the name";
+                    }
+                    return null;
+                  },
+                  onSaved: (String name) {
+                    widget.name = name;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: kInputDecoration.copyWith(hintText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    widget.email = value;
+                  },
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Enter the email";
+                    }
+                    if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                        .hasMatch(value)) {
+                      return 'Please enter valid Email';
+                    }
+                    return null;
+                  },
+                  onSaved: (String email) {
+                    widget.email = email;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: kInputDecoration.copyWith(hintText: 'Password'),
+                  onChanged: (value) {
+                    widget.password = value;
+                  },
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Enter the Password";
+                    } else if (value.length < 8) {
+                      return "Password should be atleast 8 characters";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (String password) {
+                    widget.password = password;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  children: [
+                    Text('Date of Birth:'),
+                    SizedBox(
+                      width: 50.0,
+                    ),
+                    Material(
+                      borderRadius: BorderRadius.circular(30.0),
+                      color: Colors.grey,
+                      child: MaterialButton(
+                        child: Text(
+                          dob,
+                        ),
+                        onPressed: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1950),
+                            lastDate: DateTime.now(),
+                          ).then((d) {
+                            DateFormat formatter = DateFormat('dd-MM-yyyy');
+                            String date = formatter.format(d);
+                            widget.dob = date;
+                            setState(() {
+                              dob = date;
+                            });
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Text('Gender : '),
+                      Radio(
+                          value: 'male',
+                          groupValue: widget.gender,
+                          onChanged: (value) {
+                            setState(() {
+                              widget.gender = value;
+                            });
+                          }),
+                      Text('Male'),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Radio(
+                          value: 'female',
+                          groupValue: widget.gender,
+                          onChanged: (value) {
+                            setState(() {
+                              widget.gender = value;
+                            });
+                          }),
+                      Text('Female'),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Material(
                   borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.grey,
+                  color: Colors.blue,
+                  elevation: 5.0,
                   child: MaterialButton(
                     child: Text(
-                      dob,
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
+                    height: 40,
+                    minWidth: 20,
                     onPressed: () {
-                      showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(1950),
-                        lastDate: DateTime.now(),
-                      ).then((d) {
-                        DateFormat formatter = DateFormat('dd-MM-yyyy');
-                        String date = formatter.format(d);
-                        widget.dob = date;
-                        setState(() {
-                          dob = date;
-                        });
-                      });
+                      if (_formkey.currentState.validate() && widget.dob!='' && widget.gender!='') {
+                        registerUser();
+                      } else {
+                        print('Incomplete field');
+                      }
                     },
                   ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Row(
-                children: [
-                  Text('Gender : '),
-                  Radio(
-                      value: 'male',
-                      groupValue: widget.gender,
-                      onChanged: (value) {
-                        setState(() {
-                          widget.gender = value;
-                        });
-                      }),
-                  Text('Male'),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Radio(
-                      value: 'female',
-                      groupValue: widget.gender,
-                      onChanged: (value) {
-                        setState(() {
-                          widget.gender = value;
-                        });
-                      }),
-                  Text('Female'),
-                ],
-              )),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Material(
-              borderRadius: BorderRadius.circular(30.0),
-              color: Colors.blue,
-              elevation: 5.0,
-              child: MaterialButton(
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                height: 40,
-                minWidth: 20,
-                onPressed: () {
-                  registerUser();
-                },
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
