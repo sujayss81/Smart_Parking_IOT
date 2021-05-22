7eacf938a9ad100342ae6c685d34e82c3b7587b1
Stalin <stalins205@gmail.com>
2021-05-22 16:39:38 +0530

    updated the post and get request method for login screen

--- smart_parking_app/lib/screens/homeScreen.dart
+++ smart_parking_app/lib/screens/homeScreen.dart
@@ -9,45 +9,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   static const id = 'home_screen';
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String email = '';
-  String password = '';
+  String _email = '';
+  String _password = '';
   final emailText = TextEditingController();
   final passwordText = TextEditingController();
   SharedPreferences prefs;
-
-  void postRequest({String urlLabel, Map body}) async {
-    String sendingBody = jsonEncode(body);
-    print(sendingBody);
-    String token;
-    http.Response res = await http.post(Uri.parse('$hostUrl/$urlLabel'),
-        body: sendingBody, headers: {'Content-Type': 'application/json'});
-    if (res.statusCode == 200) {
-      token = res.headers['x-auth-token'];
-      print(token);
-      prefs.setString('token', token);
-      http.Response res2 = await http.get(Uri.parse('$hostUrl/me'),
-          headers: {'x-auth-token': token, 'Content-Type': 'application/json'});
-      if (res2.statusCode == 200) {
-        // print(res2);
-        print(res2.body);
-      } else if (res2.statusCode == 400) {
-        print('authentication problem');
-        // print(res.body);
-      } else {
-        print('server error');
-      }
-    } else if (res.statusCode == 400) {
-      print('post request authentication error');
-    } else {
-      print('post request server error');
-      print(res.statusCode);
-    }
-  }
+  final _formKey = GlobalKey<FormState>();
+  bool flag = false;
+  Networking net = new Networking();
 
   void initSharedPref() async {
     prefs = await SharedPreferences.getInstance();
@@ -82,56 +57,120 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: emailText,
-                decoration:
-                    kInputDecoration.copyWith(hintText: 'Enter your email'),
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: passwordText,
-                obscureText: true,
-                decoration:
-                    kInputDecoration.copyWith(hintText: 'Enter your password'),
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Material(
-                borderRadius: BorderRadius.circular(30.0),
-                color: Colors.blue,
-                elevation: 5.0,
-                child: MaterialButton(
-                  child: Text('Login'),
-                  height: 40,
-                  minWidth: 20,
-                  onPressed: () {
-                    // emailText.clear();
-                    // passwordText.clear();
-                    Map body = {'email': email, 'password': password};
-                    String token = prefs.getString('token');
-                    if (token == null) {
-                      postRequest(urlLabel: 'login', body: body);
-                    }
-                  },
-                ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      // controller: emailText,
+                      decoration: kInputDecoration.copyWith(
+                          hintText: 'Enter your email'),
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.emailAddress,
+                      // autovalidate: true,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your email';
+                        } else if (!RegExp(
+                                "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                            .hasMatch(value)) {
+                          return 'Please enter valid email';
+                        } else
+                          return null;
+                      },
+                      // onSaved: (email) {
+                      //   _email = email;
+                      // },
+                      onChanged: (value) {
+                        _email = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      // controller: passwordText,
+                      obscureText: true,
+                      decoration: kInputDecoration.copyWith(
+                          hintText: 'Enter your password'),
+                      textAlign: TextAlign.center,
+                      // autovalidate: true,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your password';
+                        } else
+                          return null;
+                      },
+                      // onSaved: (password) {
+                      //   _password = password;
+                      // },
+                      onChanged: (value) {
+                        _password = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.blue,
+                    elevation: 5.0,
+                    child: MaterialButton(
+                      child: Text('Login'),
+                      height: 40,
+                      minWidth: 250,
+                      onPressed: () async {
+                        // emailText.clear();
+                        // passwordText.clear();
+                        print(_email);
+                        print(_password);
+                        if (_formKey.currentState.validate()) {
+                          Map body = {'email': _email, 'password': _password};
+                          print(body);
+                          String token = prefs.getString('token');
+                          // prefs.clear();
+                          if (token == null) {
+                            http.Response res = await net.postRequest(
+                                urlLabel: 'login', body: body);
+                            if (res.statusCode == 200) {
+                              token = res.headers['x-auth-token'];
+                              print(token);
+                              prefs.setString('token', token);
+                              prefs.clear();
+                              flag = false;
+                            } else if (res.statusCode == 400) {
+                              //add a toast specifying that username or password is incorrect
+                              flag = true;
+                              print('Authenticaton failed');
+                            } else {
+                              print('server error');
+                              print(jsonDecode(res.body));
+                              flag = true;
+                            }
+                          }
+                          if (!flag) {
+                            http.Response res2 = await net.getRequest(
+                                urlLabel: 'me', token: token, params: '');
+                            if (res2.statusCode == 200) {
+                              // print(res2);
+                              print(res2.body);
+                              //write navigator push code here
+                            } else {
+                              print('server error');
+                              print(jsonDecode(res2.body));
+                            }
+                          }
+                        } else {
+                          print("Incomplete fields");
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
             TextButton(
--- smart_parking_app/lib/services/networking.dart
+++ smart_parking_app/lib/services/networking.dart
@@ -1,20 +1,26 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smart_parking_app/constants.dart';
 
-// const hostUrl = 'http://sujayhome.ddns.net:4000/';
+class Networking {
+  Future<http.Response> getRequest(
+      {@required String urlLabel, String token, String params}) async {
+    String url = '$hostUrl/$urlLabel?$params';
+    http.Response res = await http.get(Uri.parse(url),
+        headers: {'x-auth-token': token, 'Content-Type': 'application/json'});
+    return res;
+  }
 
-// class Networking {
-//   Future postRequest({String urlLabel, Map body}) async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     String token = prefs.getString('token');
-//     if (token == null) {
-//       String sendingBody = jsonEncode(body);
-//       http.Response res =
-//           await http.post(Uri.parse('$hostUrl/$urlLabel'), body: sendingBody);
-//       if (res.statusCode == 200) {
-//         var tokenReceived=res.hea
-//       }
-//     }
-//   }
-// }
+  Future<http.Response> postRequest(
+      {@required String urlLabel, Map body}) async {
+    String sendingBody = jsonEncode(body);
+    print(sendingBody);
+    String token;
+    http.Response res = await http.post(Uri.parse('$hostUrl/$urlLabel'),
+        body: sendingBody, headers: {'Content-Type': 'application/json'});
+    var resBodyString = jsonDecode(res.body);
+    return res;
+  }
+}
