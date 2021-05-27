6dfca5372980cb939ff4257b7bf944b10d8d2a2e
Sujay Shenoy <sujay.ss81@gmail.com>
2021-05-27 17:33:02 +0530

    Code Refactored

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,6 @@ const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 const app = express();
-const jwt = require("jsonwebtoken");
 
 //debuggers
 const server_debug = require("debug")("server");
@@ -16,14 +15,14 @@ const hw_debug = require("debug")("hardware");
 
 //custom functions
 const connectDb = require("./utility/connectToDatabase");
-const { dropGates, raiseGates } = require("./utility/gateControl");
-const generatecode = require("./utility/generateCode");
-const { encrypt, compareHash } = require("./utility/hashing");
-const { signupSchema, loginSchema } = require("./utility/validator");
 
-//models
-const Reservation = require("./model/reservation");
-const User = require("./model/user");
+//Controllers
+const { me, login, signup } = require("./controller/userController");
+const {
+  sensorStatus,
+  reserve,
+  release,
+} = require("./controller/hardwareController");
 
 //middlewares
 const auth = require("./middleware/auth");
@@ -47,242 +46,38 @@ app.use(cors(corsOpts));
 //-------------------------------------ROUTES-----------------------------------//
 // -------GET-------//
 
-app.get("/test", auth, (req, res) => {
-  var obj = [];
-  for (let i = 0; i < 6; i++)
-    obj.push({
-      spot_number: i + 1,
-      value: i % 2 == 0 ? true : false,
-      reserved: i % 2 == 0 ? true : false,
-    });
-  return res.send({ status: "ok", message: "Sensor status array", body: obj });
-});
-
 app.get("/", (req, res) => {
   res.send({
-    api: "Smart-Parking-System",
-    version: "1.0.0",
+    api: process.env.APP_NAME,
+    version: process.env.APP_VER,
     status: "working",
   });
 });
 
-app.get("/me", auth, async (req, res) => {
-  const email = req.body.decoded.email;
-  try {
-    var result = await User.findOne({ email: email }).select([
-      "-_id",
-      "-password",
-      "-__v",
-    ]);
-  } catch (ex) {
-    db_debug(result);
-  }
-  res.send({ status: "ok", message: "User Profile", body: result });
-});
-
-app.get("/sensorStatus", auth, async (req, res) => {
-  var sensor_status;
-  var tcp_con = global.tcpCon;
+app.get("/test", auth, (req, res) => {
   var obj = [];
-  try {
-    await new Promise((resolve, reject) => {
-      tcp_con.write("1");
-      tcp_con.on("data", async (d) => {
-        sensor_status = d.split(",");
-        let sum = 0;
-        for (var i = 0; i < sensor_status.length; i++) {
-          sum += parseInt(sensor_status[i]);
-          if (sum == 0) {
-            reject();
-          } else {
-            for (var i = 0; i < sensor_status.length; i++) {
-              if (sensor_status[i] > 15 || sensor_status[i] == 0)
-                sensor_status[i] = false;
-              else sensor_status[i] = true;
-              var result = await Reservation.findOne({
-                spot_number: i + 1,
-              }).catch((e) => {
-                db_debug(e);
-              });
-              var status = result.status == "reserved" ? true : false;
-              obj.push({
-                spot_number: i + 1,
-                value: sensor_status[i],
-                reserved: status,
-              });
-            }
-            resolve();
-          }
-        }
-      });
+  for (let i = 0; i < 5; i++)
+    obj.push({
+      spot_number: i + 1,
+      value: i % 2 == 0 ? true : false,
+      reserved: i % 3 == 0 ? true : false,
     });
-  } catch (e) {
-    hw_debug(`ESP failed to return sensor status ${e}`);
-    return res.status(500).send({
-      status: "failed",
-      message: "No Data from hardware",
-      error: "Sensors not responding",
-    });
-  }
   return res.send({ status: "ok", message: "Sensor status array", body: obj });
 });
 
-app.get("/reserve", auth, async (req, res) => {
-  var { spot } = req.query;
-  if (spot == null) {
-    return res.status(400).send({
-      status: "failed",
-      message: "spot parameter required",
-    });
-  }
-  var reserve_doc = await Reservation.findOne({ spot_number: spot }).catch(
-    (ex) => db_debug(ex)
-  );
-  if (reserve_doc.code != null) {
-    return res.send({
-      status: "reserved",
-      message: "Spot already reserved",
-    });
-  }
+app.get("/me", auth, me);
 
-  var opr_res = await dropGates(spot);
-  if (opr_res) {
-    var code = generatecode();
-    await Reservation.updateOne(
-      { spot_number: spot },
-      { $set: { status: "reserved", code: code } }
-    ).catch((e) => db_debug(e));
+app.get("/sensorStatus", auth, sensorStatus);
 
-    var millis = process.env.RESERVATION_EXPIRY * 60000;
+app.get("/reserve", auth, reserve);
 
-    res.send({ status: "ok", message: "Spot Reserved", code: code });
-
-    setTimeout(async () => {
-      await Reservation.updateOne(
-        { spot_number: spot },
-        { $set: { status: "not-reserved", code: null } }
-      ).catch((e) => db_debug(e));
-      if (await raiseGates(spot)) {
-        server_debug("Reservation invoked due to timeout");
-      }
-    }, millis);
-  }
-});
-
-app.get("/release", auth, async (req, res) => {
-  var { spot, code } = req.query;
-  if (spot == null) {
-    return res.status(400).send({
-      status: "failed",
-      message: "spot parameter required",
-    });
-  } else if (code == null) {
-    return res.status(400).send({
-      status: "failed",
-      message: "code parameter required",
-    });
-  }
-  var result = await Reservation.findOne({ spot_number: spot }).catch((e) =>
-    db_debug(e)
-  );
-  if (code == result.code) {
-    if (await raiseGates(spot)) {
-      await Reservation.updateOne(
-        { spot_number: spot },
-        {
-          $set: {
-            status: "not-reserved",
-            code: null,
-          },
-        }
-      ).catch((e) => db_debug(e));
-    }
-  } else {
-    return res.status(400).send({
-      status: "failed",
-      message: "invalid code",
-    });
-  }
-  return res.send({
-    status: "ok",
-    message: "spot Released",
-  });
-});
+app.get("/release", auth, release);
 
 // --------POST--------//
 
-app.post("/login", async (req, res) => {
-  const val_res = loginSchema.validate(req.body);
-  if (val_res.error == null) {
-    var { email, password } = req.body;
-    try {
-      var result = await User.find({
-        email: email,
-      });
-    } catch {
-      (ex) => {
-        db_debug(ex);
-      };
-    }
-    if (result.length > 0) {
-      if (await compareHash(password, result[0].password)) {
-        var token = jwt.sign({ email: email }, process.env.JWT_SECRET);
-        return res.header({ "x-auth-token": token }).send({
-          status: "ok",
-          message: "Authentication successfull",
-        });
-      }
-    }
-  } else {
-    server_debug("Missing params");
-    return res.status(400).send({
-      status: "failed",
-      message: "Missing/Incorrect Parameters",
-      body: val_res.error.details,
-    });
-  }
-  server_debug("Incorrect email/pass");
-  return res.status(400).send({
-    status: "failed",
-    message: "Incorrect Email/Password",
-  });
-});
+app.post("/login", login);
 
-app.post("/signup", async (req, res) => {
-  var val_res = await signupSchema.validate(req.body);
-  if (val_res.error == null) {
-    var { name, email, password, dob, gender } = req.body;
-    password = await encrypt(password);
-    email = email.toLowerCase();
-    var user = new User({ name, email, password, dob, gender });
-    await user
-      .save()
-      .then(() => {
-        var token = jwt.sign({ email: email }, process.env.JWT_SECRET);
-        return res.header({ "x-auth-token": token }).send({
-          status: "ok",
-          message: "registration successfull",
-        });
-      })
-      .catch((ex) => {
-        db_debug(ex);
-        if (ex.dupData != null && ex.dupData == 1) {
-          server_debug("Email already registered");
-          return res.status(409).send({
-            status: "failed",
-            message: "email already registered",
-          });
-        }
-      });
-  } else {
-    server_debug("Missing params");
-    return res.status(400).send({
-      status: "failed",
-      message: "Missing/Incorrect Parameters",
-      body: val_res.error.details,
-    });
-  }
-});
+app.post("/signup", signup);
 
 //----------Error-Handlers--------------//
 
---
+++ Server/controller/userController.js
@@ -0,0 +1,102 @@
+//npm imports
+const jwt = require("jsonwebtoken");
+//debuggers
+const server_debug = require("debug")("server");
+const db_debug = require("debug")("database");
+//model imports
+const User = require("../model/user");
+//custom functions
+const { encrypt, compareHash } = require("../utility/hashing");
+const { signupSchema, loginSchema } = require("../utility/validator");
+
+// ------------------------------------------------------------------- //
+
+const me = async (req, res) => {
+  const email = req.body.decoded.email;
+  try {
+    var result = await User.findOne({ email: email }).select([
+      "-_id",
+      "-password",
+      "-__v",
+    ]);
+  } catch (ex) {
+    db_debug(result);
+    return res.send({ status: "failed", message: "User Profile not found" });
+  }
+  res.send({ status: "ok", message: "User Profile", body: result });
+};
+
+const login = async (req, res) => {
+  const val_res = loginSchema.validate(req.body);
+  if (val_res.error == null) {
+    var { email, password } = req.body;
+    try {
+      var result = await User.find({
+        email: email,
+      });
+    } catch {
+      (ex) => {
+        db_debug(ex);
+      };
+    }
+    if (result.length > 0) {
+      if (await compareHash(password, result[0].password)) {
+        var token = jwt.sign({ email: email }, process.env.JWT_SECRET);
+        return res.header({ "x-auth-token": token }).send({
+          status: "ok",
+          message: "Authentication successfull",
+        });
+      }
+    }
+  } else {
+    server_debug("Missing params");
+    return res.status(400).send({
+      status: "failed",
+      message: "Missing/Incorrect Parameters",
+      body: val_res.error.details,
+    });
+  }
+  server_debug("Incorrect email/pass");
+  return res.status(400).send({
+    status: "failed",
+    message: "Incorrect Email/Password",
+  });
+};
+
+const signup = async (req, res) => {
+  var val_res = await signupSchema.validate(req.body);
+  if (val_res.error == null) {
+    var { name, email, password, dob, gender } = req.body;
+    password = await encrypt(password);
+    email = email.toLowerCase();
+    var user = new User({ name, email, password, dob, gender });
+    await user
+      .save()
+      .then(() => {
+        var token = jwt.sign({ email: email }, process.env.JWT_SECRET);
+        return res.header({ "x-auth-token": token }).send({
+          status: "ok",
+          message: "registration successfull",
+        });
+      })
+      .catch((ex) => {
+        db_debug(ex);
+        if (ex.dupData != null && ex.dupData == 1) {
+          server_debug("Email already registered");
+          return res.status(409).send({
+            status: "failed",
+            message: "email already registered",
+          });
+        }
+      });
+  } else {
+    server_debug("Missing params");
+    return res.status(400).send({
+      status: "failed",
+      message: "Missing/Incorrect Parameters",
+      body: val_res.error.details,
+    });
+  }
+};
+
+module.exports = { me, login, signup };
