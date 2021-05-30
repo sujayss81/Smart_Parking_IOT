052b3ceeb56bb52718dd743e6017b4c011389ce2
Saneeth99 <66729256+Saneeth99@users.noreply.github.com>
2021-05-30 18:13:31 +0530

    All bookings UI

--- smart_parking_app/lib/screens/allBookings.dart
+++ smart_parking_app/lib/screens/allBookings.dart
@@ -7,10 +7,11 @@ import 'package:smart_parking_app/components/drawerScreen.dart';
 import 'package:smart_parking_app/services/networking.dart';
 
 class BookingScreen extends StatefulWidget {
+  List<Map> detailsList= [];
   final Map body;
   BookingScreen({this.body});
-  var details;
-  var count;
+  List details;
+  int count;
   @override
   _BookingScreenState createState() => _BookingScreenState();
 }
@@ -24,12 +25,18 @@ class _BookingScreenState extends State<BookingScreen> {
     String token = prefs.getString('token');
     http.Response res =
         await net.getRequest(urlLabel: 'myorders', token: token, params: '');
-    print(jsonDecode(res.body));
+    // print(jsonDecode(res.body));
     var detailsArray = jsonDecode(res.body);
+    List details = detailsArray['body'];
     // widget.count = detailsArray['count'];
-    // widget.details = detailsArray['body'];
-    // print(widget.details);
-    print(detailsArray);
+    widget.details = details;
+    // for(int i=0 ; i<widget.count ; i++)
+    //   {
+    //     widget.detailsList.add(detailsArray['body'][i]);
+    //   }
+    print(details);
+    // print(detailsArray);
+    setState(() {});
   }
 
   void initState() {
@@ -40,6 +47,7 @@ class _BookingScreenState extends State<BookingScreen> {
   @override
   Widget build(BuildContext context) {
     var details = widget.details;
+    print(widget.count);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -48,15 +56,87 @@ class _BookingScreenState extends State<BookingScreen> {
         drawer: DrawerScreen(
           data: widget.body,
         ),
-        // body: ListView.builder(
-        //   itemCount: widget.count,
-        //   itemBuilder: (context, index) {
-        //     print('count=${widget.count}');
-        //     return Card(
-        //       child: Text('hello'),
-        //     );
-        //   },
-        // ),
+        body: ListView.builder(
+          itemCount: widget.details.length,
+          itemBuilder: (context, index) {
+            print('details=${widget.details}');
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Card(
+                elevation: 5,
+                color: Colors.grey[200],
+                child:Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Parking Slot',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                            ),
+
+                            Text('Code',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(widget.details[index]['spot_number'].toString(),
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue
+                              ),
+                            ),
+
+                            Text(widget.details[index]['code'].toString(),
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(widget.details[index]['createdAt'].toString().substring(0,10),
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                ),
+                            ),
+                            Text(widget.details[index]['createdAt'].toString().substring(11,16),
+                              style: TextStyle(
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
